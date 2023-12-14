ca248bfc08a332bfc954f4dbe3e96e1812f1a7f3
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-12-14 13:22:04 -0300

    create filter button

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = 'Testando filtro';
 
   constructor(private service: PensamentoService) {}
 
@@ -21,12 +22,11 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos.push(...listaPensamentos);
-        if(!listaPensamentos.length) {
-          this.haMaisPensamentos = false
-        }
-      })
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
